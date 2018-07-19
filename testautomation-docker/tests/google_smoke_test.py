583f23c47ed4dd87e535d0776ceab01b0940a522
me <me@someplace.io>
2018-07-19 15:34:18 +0000

    renamed to pytest naming convention

--- testautomation-docker/tests/google_smoke_test.py
+++ testautomation-docker/tests/google_smoke_test.py
@@ -1,15 +1,17 @@
+import sys
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-class GoogleTestCase(unittest.TestCase):
+class TestGoogleConn(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Remote(command_executor='http://selenium-hub.grid:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME) 
+	hub_hostname=sys.argv[1]
+        self.browser = webdriver.Remote(command_executor='http://' + str(hub_hostname) + ':4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME) 
         self.addCleanup(self.browser.quit)
 
-    def testPageTitle(self):
+    def test_PageTitle(self):
         self.browser.get('http://www.google.com')
         self.assertIn('Google', self.browser.title)
 
